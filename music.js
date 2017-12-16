9e45bff27ede641e5f57cd824996a66b4c9c3eb0
hedongsheng <he77505357@163.com>
2017-12-16 18:44:29 +0800

    Update music.js

--- music.js
+++ music.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET','/music/music.json',true);
+xhr.open('GET','https://easy-mock.com/mock/5a34f6d8b3739f4feec6f314/music/he-music',true);
 xhr.send();
 xhr.onload = function(){
     console.log(JSON.parse(xhr.responseText));
