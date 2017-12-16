a2a9922c89c5762d8124810f1649ac9786e8e399
hedongsheng <he77505357@163.com>
2017-12-16 16:34:46 +0800

    Update music.js

--- music.js
+++ music.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GEt','/music.json',true);
+xhr.open('GET','/music.json',true);
 xhr.send();
 xhr.onload = function(){
     console.log(JSON.parse(xhr.responseText));
